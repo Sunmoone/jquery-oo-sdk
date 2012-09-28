a9d5f938b1622483874c16d7fa0828badd474e24
jslover <fzuhua@gmail.com>
2012-09-28 17:03:42 +0800

    v1.0
    发布基本代码
    包含两个简单的组件示例

---
+++ odk/code.js
@@ -0,0 +1,151 @@
+﻿/**
+* @static
+* @description 基于jQuery的OO框架
+* @author http://jslover.com
+* @version 1.0  20120928
+*/
+var ODK = (function (win, doc, $) {
+    //所有组件的基类
+    var BaseClass = function () {
+    };
+    BaseClass.prototype = {
+        init: function () {
+            //接口，基类无实现
+        }
+        , render: function () {
+            //接口，基类无实现
+        }
+        , unrender: function () {
+            //接口，基类无实现
+        }
+    };
+    //构造器
+    var _ODK = function () {
+        //UI控件
+        this.UI = {};
+        //全局缓存变量
+        this.Cache = {};
+        //延时变量
+        this.Timer = {};
+        //api队列
+        this.ajaxQueue = [];
+        this.ajaxReady = true;
+        this.IE6 = !!window.ActiveXObject && !window.XMLHttpRequest;
+    };
+    _ODK.prototype = {
+        init: function () {
+            this.regClassExtend();
+        }
+        //所有组件的基类
+        , BaseClass: BaseClass
+        //注册类的继承方法
+        , regClassExtend: function () {
+            if (Function.prototype.extend) { return; }
+            Function.prototype.extend = function (superClass) {
+                if (typeof superClass === 'function') {
+                    //类式继承  
+                    var F = function () { };
+                    F.prototype = superClass.prototype;
+                    this.prototype = new F();
+                    //设置构造函数指向自己 
+                    this.prototype.constructor = this;
+                    //同时，添加一个指向父类构造函数的引用
+                    this.superClass = superClass;
+                }
+                return this;
+            };
+        }
+        //注册组件
+        , regUI: function (uiName, ui) {
+            if (uiName.indexOf('.') < 0) {
+                this.UI[uiName] = ui;
+            } else {
+                ui_parent = uiName.split('.')[0];
+                ui_child = uiName.split('.')[1];
+                if (!this.UI[ui_parent]) {
+                    this.UI[ui_parent] = {};
+                }
+                this.UI[ui_parent][ui_child] = ui;
+            }
+        }
+        //获取自增ID
+        , getAutoIncrementID: function () {
+            if (!ODK.Cache.autoIncrementID) {
+                ODK.Cache.autoIncrementID = 99;
+            }
+            return ++ODK.Cache.autoIncrementID;
+        }
+        //排队执行AJAX，确保每次只有一个请求
+		, singleAjax: function (url, param, callBack, type, cache, onerror) {
+		    return;
+		    if (typeof type == "undefined") {
+		        type = 'GET'
+		    }
+		    //加入队列
+		    ODK.ajaxQueue.push({
+		        apiUrl: url
+                , param: param
+                , callBack: callBack
+                , type: type
+                , cache: cache
+                , onerror: onerror
+		    });
+		    runAjax();
+		}
+    };
+    //执行AJAX
+    function runAjax() {
+        if (ODK.ajaxQueue.length == 0 || !!ODK.ajaxBusy) {
+            return;
+        }
+        //状态锁
+        ODK.ajaxBusy = true;
+        var p = $.extend({}, ODK.ajaxQueue[0]);
+        try {
+            $.ajax({
+                url: p.apiUrl
+            , type: p.type
+            , cache: p.cache || false
+            , complete: function () {
+                ODK.ajaxBusy = false;
+                //递归，继续调用
+                runAPI();
+            }
+            , data: p.param
+            , success: function (data) {
+                try {
+                    if (p.callBack && data) {
+                        p.callBack(data);
+                    }
+                } catch (ee) {
+                    onError(e, onerror);
+                }
+            }
+            , error: function (e) {
+                onError(e, onerror);
+            }
+            , dataType: 'json'
+            });
+        } catch (ee) {
+            onError(ee, onerror);
+        }
+
+        //删除数组第一项
+        ODK.ajaxQueue.shift();
+    }
+    //全局错误处理
+    function onError(e, fn) {
+        //console.log(e);
+        if (fn) {
+            fn(e);
+        }
+    }
+    //确保单例
+    if (!win.___ODK) {
+        win.___ODK = new _ODK();
+    }
+    return win.___ODK;
+})(window, document, jQuery);
+
+/*默认初始化*/
+ODK.init();
---
+++ odk/ui/box/box.js
@@ -0,0 +1,83 @@
+﻿/**
+* @class
+* @description 所有弹出框的基类 仅作为测试DEMO
+* @author http://jslover.com
+* @version 1.0  20120928
+* @param {Object} JSON格式参数
+*/
+(function (ODK, $, win) {
+    //所有弹出框的基类-继承至BaseClass
+    var Box = function (opts) {
+        //初始化父类
+        Box.superClass.call(this);
+        //参数合并
+        this.opts = $.extend({}, defaults, opts);
+        //缓存当前实例
+        this.$this = null;
+        this.init();
+
+    }
+    //继承
+    Box.extend(ODK.BaseClass);
+    //默认参数
+    var defaults = {
+        //是否自动生成
+        autoRender: true
+        , title: '这是一个窗体'
+        , content: '这是内容'
+    };
+    //扩展方法
+    var fn = {
+        //重写父类的init方法
+        init: function () {
+            var _this = this;
+            if (_this.opts.autoRender) {
+                //生成界面
+                _this.render();
+                //事件绑定
+                _this.bindEvent();
+            }
+        }
+        //生成界面
+        , render: function () {
+            var _this = this;
+            var h = templete.getBox(_this.opts);
+            _this.$this = $(h).appendTo(document.body);
+            //....
+        }
+        //注册事件
+        , bindEvent: function () {
+            var _this = this;
+            _this.$this.find('.odk-ui-box-close').click(function () {
+                _this.hide();
+            });
+        }
+        , show: function () {
+            this.$this.show();
+        }
+        , hide: function () {
+            this.$this.hide();
+        }
+        , setContent: function (content) {
+            this.$this.find('.odk-ui-box-content').html(content);
+        }
+        , setTitle: function (title) {
+            this.$this.find('.odk-ui-box-title').html(title);
+        }
+    };
+    //模板
+    var templete = {
+        getBox: function (opts) {
+            var h = '';
+            h += '<div class="odk-ui-box">';
+            h += '<h1><a href="#" class="odk-ui-box-close">关闭</a><span class="odk-ui-box-title">' + opts.title + '</span></h1>';
+            h += '<p class="odk-ui-box-content">' + opts.content + '</p>'
+            h += '</div>';
+            return h;
+        }
+    };
+    //合并扩展方法
+    $.extend(Box.prototype, fn);
+    //注册组件 ODK.UI.Box
+    ODK.regUI("Box", Box);
+})(ODK, jQuery, window);
